8d4ad231988b4683483ae3830974512733e79dec
KawaichMag <kawaichmag@gmail.com>
2026-04-24 20:54:27 +0300

    feat: script for automated data transition

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+password=$(head -n 1 secrets/.psql.pass)
+
+hdfs dfs -rm -r -f project/warehouse
+
+sqoop import-all-tables --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team15_projectdb --username team15 --password $password --compression-codec=snappy --compress --as-avrodatafile --warehouse-dir=project/warehouse --m 1
