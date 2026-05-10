54a2be6cb813c960e7df08840ebbbab2963b1c28
KawaichMag <kawaichmag@gmail.com>
2026-05-11 02:33:20 +0300

    fix: fixed rm folders in stage1

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -rf output/
+rm -rf output/warehouse/trips output/warehouse/vehicles
 
 if [ -f "data/VED_171101_week.csv" ]; then
     echo "Dataset already Loaded.. Skipping this part"
